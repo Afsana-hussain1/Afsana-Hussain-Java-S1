fc6abbbcecb2d8a4a6ad329e094f5e09c568b4f7
Afsana Hussain <aahussain1230@gmail.com>
2019-12-04 10:48:53 -0500

    summative

--- summative 120419/summative 9/starter-code/comment-service/src/main/java/com/trilogyed/comment/controller/CommentController.java
+++ summative 120419/summative 9/starter-code/comment-service/src/main/java/com/trilogyed/comment/controller/CommentController.java
@@ -1,12 +1,68 @@
 package com.trilogyed.comment.controller;
-
-
+import com.sun.jersey.api.NotFoundException;
+import com.trilogyed.comment.dao.CommentDao;
+import com.trilogyed.comment.model.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.util.List;
+
+@RequestMapping("/comments")
 @RestController
 @RefreshScope
-@RequestMapping(value = "/comment")
 public class CommentController {
+    @Autowired
+    CommentDao dao;
+
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+
+    public Comment addComment(@Valid @RequestBody Comment comment) {
+
+        return dao.addComment(comment);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Comment getComment(@PathVariable int id) {
+
+        Comment comment = dao.getComment(id);
+
+        if(comment == null){
+            throw new NotFoundException(("No Comment with this id exists"));
+        }
+        return dao.getComment(id);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Comment> getAllComments() {
+
+        return dao.getAllComments();
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateComment(@Valid @RequestBody Comment comment, @PathVariable int id) {
+
+        dao.updateComment(comment);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteComment(@PathVariable int id) {
+
+        Comment comment = dao.getComment(id);
+
+        if(comment == null){
+            throw new NotFoundException(("No Comment with this id exists"));
+        }
+        dao.deleteComment(id);
+    }
+
+
 }
--- summative 120419/summative 9/starter-code/post-service/src/main/java/com/trilogyed/post/controller/PostController.java
+++ summative 120419/summative 9/starter-code/post-service/src/main/java/com/trilogyed/post/controller/PostController.java
@@ -1,30 +1,71 @@
 package com.trilogyed.post.controller;
 
+import com.trilogyed.post.dao.PostDao;
 import com.trilogyed.post.model.Post;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RequestMapping("/posts")
+@RestController
+@RefreshScope
 public class PostController {
 
+    @Autowired
+    PostDao postdao;
+
     @RequestMapping(value = "/posts", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED)
     public Post createPost(@RequestBody Post post) {
-        return null;
+        return postdao.addPost(post);
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
     public Post getPost(@PathVariable int id) {
-        return null;
+        Post post = postdao.getPost(id);
+
+        if(post == null){
+            throw new NotFoundException(("the post w/that id doesnt exist"));
+        }
+        return postdao.getPost(id);
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.PUT)
+
     public void updatePost(@RequestBody Post post, @PathVariable int id) {
+        postdao.updatePost(post);
 
     }
 
     @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deletePost(@PathVariable int id) {
+        Post post = postdao.getPost(id);
+
+        if(post == null){
+            throw new NotFoundException(("that post does not exist"));
+        }
+        postdao.deletePost(id);
 
     }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Post> getAllPosts() {
+
+        return postdao.getAllPosts();
+    }
+
+    @GetMapping("/user/{posterName}")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Post> getPostsByUser(@PathVariable String posterName) {
+
+        return postdao.getPostsByUser(posterName);
+    }
 }
+
+
